a091d00ab72d1e063d2b411852ea3192edbc590a
Pavel Lupashko <pavel.lupashko228@gmail.com>
2023-02-24 14:55:42 +0200

    progressBar

--- js/audio.js
+++ js/audio.js
@@ -1,19 +1,16 @@
 'use strict'
-<<<<<<< HEAD
-/*
+
 var audio = document.getElementById("sword-hit-1");
-=======
 
 function change() {
-    var checkbox = document.getElementById("switch-music");
->>>>>>> f059cea1c6fdc963c583770dcc918de025584806
+    var checkbox = document.getElementById("switch-music"); 
 
     var music = document.getElementById("myAudio");
     music.volume = 0.1;
     if (checkbox.checked == true) {
-      music.play();
+        music.play();
     } else {
-      music.pause();
+        music.pause();
     }
 }
 
@@ -33,9 +30,4 @@ document.addEventListener("click", function(event) {
     const newAudio = new Audio('./sounds/sword-hit-1.mp3');
     newAudio.volume = 0.3;
     newAudio.play();
-});
-<<<<<<< HEAD
-*/
-=======
-
->>>>>>> f059cea1c6fdc963c583770dcc918de025584806
+}); 
--- js/main.js
+++ js/main.js
@@ -1,17 +1,34 @@
 var _damageSword = 10;
+var HP_pers = 100;
+var HP_chydo = 250;
+var width = 100;
+ 
+function move() {
+    var elem = document.getElementById("myBar");    
+    var proc = (100 * _damageSword) / HP_chydo; 
+    width -= proc;
+    elem.style.width = width + '%'; 
+    elem.innerHTML = width * 1  + '%'; 
+}
 
 
+
+
+
+/*
 function move() {
     var elem = document.getElementById("myBar");   
     var width = 100;
     var id = setInterval(frame, 10);
-    function frame() {
-        if (width <= 20) {
+    function frame() { 
+        if (width <= HP_chydo - _damageSword) {
         clearInterval(id);
-        } else {
+        HP_chydo = HP_chydo - _damageSword;
+        } 
+        else {
         width -= _damageSword; 
         elem.style.width = width + '%'; 
         elem.innerHTML = width * 1  + '%';
         }
     }
-}
+}*/
