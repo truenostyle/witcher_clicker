d4539c4a4459d76c4094cb89229e06c41bbb9916
axxvxvx <80685257+jigsaw26@users.noreply.github.com>
2023-02-24 13:14:26 +0200

    music

--- js/audio.js
+++ js/audio.js
@@ -1,12 +1,25 @@
 'use strict'
 
-var audio = document.getElementById("sword-hit-1");
+function change() {
+    var checkbox = document.getElementById("switch-music");
 
-function playAudio() {
-    audio.play();
-}
+    var music = document.getElementById("myAudio");
+    music.volume = 0.1;
+    if (checkbox.checked == true) {
+      music.play();
+    } else {
+      music.pause();
+    }
+  }
+
+function playMusic() {
+    const music = new Audio('./sounds/music.mp3');
+    music.volume = 0.1;
+    music.play();
+  }
 
 document.addEventListener("click", function(event) {
     const newAudio = new Audio('./sounds/sword-hit-1.mp3');
+    newAudio.volume = 0.3;
     newAudio.play();
 });
