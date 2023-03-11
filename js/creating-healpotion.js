1a17c4594a08754834ede61c5e6482f57b152251
axxvxvx <80685257+jigsaw26@users.noreply.github.com>
2023-03-11 14:44:32 +0200

    меню+настройки

--- js/creating-healpotion.js
+++ js/creating-healpotion.js
@@ -1,113 +1,132 @@
 'use strict'
 
-let dmgimages = [];
-let hp_count = 0;
-let dmg_count = 0;
-for (let i = 0; i < 5; i++) {
-    let img2 = new Image(); 
-    img2.src = './photo/items/dmgpotion.png'; 
-    dmgimages.push(img2); 
-}
-
-for(let i = 0; i < 5; i++)
+let game_conteiner = document.getElementById("game_conteiner");
+game_conteiner.style.display = 'none';
+let intervalMENU = setInterval(checkMENU, 3000);
+    
+    
+function checkMENU()
 {
-    dmgimages[i].style.display = 'none';
-    dmgimages[i].style.position = "absolute";
-    dmgimages[i].style.width = "100px";
-    dmgimages[i].style.height = "100px";
-    dmgimages[i].style.top = Math.floor(Math.random() * (window.innerHeight - "100")) + "px";
-    dmgimages[i].style.left = Math.floor(Math.random() * (window.innerWidth  - "100")) + "px";
-    document.body.appendChild(dmgimages[i]);
+    if (game_conteiner.style.visibility === "collapse")
+    {
+        creating_items(); 
+        clearInterval(intervalMENU);
+    }
 }
 
 
-for (let i = 0; i < dmgimages.length; i++)
+
+function creating_items()
 {
-    dmgimages[i].addEventListener("click", function(event) {
-        const healAudio = new Audio('./sounds/drink.mp3');
-        healAudio.volume = 0.3;
-        healAudio.play();
+    let dmgimages = [];
+    let hp_count = 0;
+    let dmg_count = 0;
+    for (let i = 0; i < 5; i++) {
+        let img2 = new Image(); 
+        img2.src = './photo/items/dmgpotion.png'; 
+        dmgimages.push(img2); 
+    }
+    
+    for(let i = 0; i < 5; i++)
+    {
         dmgimages[i].style.display = 'none';
-        pers.damage = pers.damage + 10;
-    });
-}
-
-let intervalDMG = setInterval(checkDMG, 3000);
-
-
-function checkDMG()
-{
-    if (dmg_count <= dmgimages.length)
-    {
-    if(Math.round(Math.random()) === 1)
-        {
-            dmgimages[dmg_count].style.display = 'block';
-            dmg_count++;
-        }  
+        dmgimages[i].style.position = "absolute";
+        dmgimages[i].style.width = "100px";
+        dmgimages[i].style.height = "100px";
+        dmgimages[i].style.top = Math.floor(Math.random() * (window.innerHeight - "100")) + "px";
+        dmgimages[i].style.left = Math.floor(Math.random() * (window.innerWidth  - "100")) + "px";
+        document.body.appendChild(dmgimages[i]);
     }
-    else {
-        clearInterval(intervalDMG);
-    }
-}
-
-
-let images = []; 
-
-
-for (let i = 0; i < 5; i++) {
-    let img = new Image(); 
-    img.src = './photo/items/chicken.png'; 
-    images.push(img); 
-}
-
-
-for(let i = 0; i < 5; i++)
-{
-    images[i].style.display = 'none';
-    images[i].style.position = "absolute";
-    images[i].style.width = "100px";
-    images[i].style.height = "100px";
-    images[i].style.top = Math.floor(Math.random() * (window.innerHeight - "100")) + "px";
-    images[i].style.left = Math.floor(Math.random() * (window.innerWidth  - "100")) + "px";
-    document.body.appendChild(images[i]);
-}
-
-let intervalHP = setInterval(checkHP, 3000);
-
-function checkHP()
-{
-    if (hp_count <= images.length)
+    
+    
+    for (let i = 0; i < dmgimages.length; i++)
     {
-        if(pers.hp <= 35)
+        dmgimages[i].addEventListener("click", function(event) {
+            const healAudio = new Audio('./sounds/drink.mp3');
+            healAudio.volume = 0.3;
+            healAudio.play();
+            dmgimages[i].style.display = 'none';
+            pers.damage = pers.damage + 10;
+        });
+    }
+    
+    let intervalDMG = setInterval(checkDMG, 3000);
+    
+    
+    function checkDMG()
+    {
+        if (dmg_count <= dmgimages.length)
         {
-            images[hp_count].style.display = 'block';
-            hp_count++;
+        if(Math.round(Math.random()) === 1)
+            {
+                dmgimages[dmg_count].style.display = 'block';
+                dmg_count++;
+            }  
+        }
+        else {
+            clearInterval(intervalDMG);
         }
     }
-    else {
-        clearInterval(intervalHP);
+    
+    
+    let images = []; 
+    
+    
+    for (let i = 0; i < 5; i++) {
+        let img = new Image(); 
+        img.src = './photo/items/chicken.png'; 
+        images.push(img); 
     }
-}
-
-
-for (let i = 0; i < images.length; i++)
-{
-    images[i].addEventListener("click", function(event) {
-        const healAudio = new Audio('./sounds/heal.mp3');
-        healAudio.volume = 0.3;
-        healAudio.play();
+    
+    
+    for(let i = 0; i < 5; i++)
+    {
         images[i].style.display = 'none';
-
-        if(pers.hp <= 80)
+        images[i].style.position = "absolute";
+        images[i].style.width = "100px";
+        images[i].style.height = "100px";
+        images[i].style.top = Math.floor(Math.random() * (window.innerHeight - "100")) + "px";
+        images[i].style.left = Math.floor(Math.random() * (window.innerWidth  - "100")) + "px";
+        document.body.appendChild(images[i]);
+    }
+    
+    let intervalHP = setInterval(checkHP, 3000);
+    
+    function checkHP()
+    {
+        if (hp_count <= images.length)
         {
-            pers.hp = pers.hp + 20;
-        } else if (pers.hp >= 81 && pers.hp <= 99)
-        {
-            pers.hp = 100;
-        } else if (pers.hp >= 100)
-        {
-            pers.hp = 100;
+            if(pers.hp <= 35)
+            {
+                images[hp_count].style.display = 'block';
+                hp_count++;
+            }
         }
-        Pers_damage(pers.hp) ;
-    });
+        else {
+            clearInterval(intervalHP);
+        }
+    }
+    
+    
+    for (let i = 0; i < images.length; i++)
+    {
+        images[i].addEventListener("click", function(event) {
+            const healAudio = new Audio('./sounds/heal.mp3');
+            healAudio.volume = 0.3;
+            healAudio.play();
+            images[i].style.display = 'none';
+    
+            if(pers.hp <= 80)
+            {
+                pers.hp = pers.hp + 20;
+            } else if (pers.hp >= 81 && pers.hp <= 99)
+            {
+                pers.hp = 100;
+            } else if (pers.hp >= 100)
+            {
+                pers.hp = 100;
+            }
+            Pers_damage(pers.hp) ;
+        });
+    }
 }
---
+++ js/menu.js
@@ -0,0 +1,104 @@
+'use strict'
+
+let menu_conteiner = document.getElementById('menu_conteiner');
+const play =  document.getElementById('play');
+const settings = document.getElementById('settings');
+
+let settings_conteiner = document.getElementById('settings_conteiner')
+let menu_buttons = document.getElementsByClassName('menu_buttons');
+
+const rangeInput = document.getElementById('volume_range');
+const rangeMusic = document.getElementById('music_range');
+let resultRangeVolume = rangeInput.value / 10;
+let musicRangeVolume = rangeMusic.value / 10;
+
+let checkSounds = document.getElementById('sounds');
+
+const brightRange = document.getElementById('bright_range');
+let brightness = brightRange.value;
+
+let theme = document.getElementById('theme');
+let menu_back = document.getElementById('menu');
+
+play.addEventListener('click', () => {
+    game_conteiner.style.display = 'block';
+    menu_conteiner.style.display = 'none';
+  });
+
+settings.addEventListener('click', () => {
+    for(let i = 0; i < menu_buttons.length; i++)
+    {
+        menu_buttons[i].style.visibility = 'hidden';
+    }
+    settings_conteiner.style.visibility = 'visible';
+});
+
+back.addEventListener('click', () => {
+    for(let i = 0; i < menu_buttons.length; i++)
+    {
+        menu_buttons[i].style.visibility = 'visible';
+    }
+    settings_conteiner.style.visibility = 'hidden';
+});
+
+rangeInput.addEventListener('input', () => {
+    resultRangeVolume = rangeInput.value;
+    if(resultRangeVolume % 1 === 0)
+        {
+            resultRangeVolume = rangeInput.value / 10;
+        }
+        else {
+            resultRangeVolume = rangeInput.value;
+        }
+    hit1Audio.volume = resultRangeVolume;
+  });
+
+rangeMusic.addEventListener('input', ()=>{
+    musicRangeVolume = rangeMusic.value / 10;
+    musicAudio.volume = musicRangeVolume;
+});
+
+
+  checkSounds.addEventListener('change', () => {
+    if (checkSounds.checked) {
+        resultRangeVolume = rangeInput.value;
+        rangeInput.disabled = false;
+        if(resultRangeVolume % 1 === 0)
+        {
+            resultRangeVolume = rangeInput.value / 10;
+        }
+        else {
+            resultRangeVolume = rangeInput.value;
+        }
+       
+    } else {
+        resultRangeVolume = 0;
+        rangeInput.disabled = true;
+    }
+  });
+
+
+brightRange.addEventListener('input', () => {
+    brightness = brightRange.value;
+    updateBrightness();
+});
+
+function updateBrightness() {
+    document.body.style.filter = `brightness(${brightness}%)`;
+  };
+  
+theme.addEventListener('change', () => {
+    if (theme.checked) {
+        menu_back.style.backgroundColor = 'rgb(216, 216, 216)';
+      for (let i = 0; menu_buttons.length;i++)
+      {
+         menu_buttons.style.backgroundColor = 'rgb(216, 216, 216)';
+      }
+    } else { 
+        for (let i = 0; menu_buttons.length;i++)
+        {
+          menu_buttons.style.backgroundColor = 'black';
+        }
+        menu_back.style.backgroundColor = 'black';
+    }
+  });
